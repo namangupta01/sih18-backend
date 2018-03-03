aab62d4fe58b7c6d4d620e453118e39e7f5a8edb
Naman Gupta <01namangupta@gmail.com>
2018-03-03 21:46:18 +0530

    Added pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303090949) do
+ActiveRecord::Schema.define(version: 20180303161006) do
+
+  create_table "admin_session_tokens", force: :cascade do |t|
+    t.string "session_token"
+    t.integer "admin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_admin_session_tokens_on_admin_id"
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
+    t.string "phone_number"
+    t.integer "admin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_id"], name: "index_admins_on_admin_id"
+  end
+
+  create_table "dams", force: :cascade do |t|
+    t.string "project_identification_code"
+    t.string "name_of_dam"
+    t.string "operated_and_maintained_by"
+    t.string "longitude_of_dam"
+    t.string "latitude_of_dam"
+    t.string "year_of_completion"
+    t.string "river_basin"
+    t.string "river"
+    t.string "nearest_city"
+    t.string "seismic_zone"
+    t.string "type_of_dam"
+    t.string "height_above_lowest_foundation"
+    t.string "length_of_dam"
+    t.string "volume_content_of_dam"
+    t.string "gross_storage_capacity"
+    t.string "reservoir_area"
+    t.string "effective_storage_capacity"
+    t.string "purpose"
+    t.string "designed_spillway_capacity"
+    t.string "state"
+    t.string "present_water_level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "directories", force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.string "dam_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_session_tokens", force: :cascade do |t|
     t.string "session_token"
