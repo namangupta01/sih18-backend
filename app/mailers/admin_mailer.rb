671724ae91daa7e014be05004e12a1d6945319af
Naman Gupta <01namangupta@gmail.com>
2018-03-22 18:21:04 +0530

    Added mailer for admin registration

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,2 +1,10 @@
 class AdminMailer < ApplicationMailer
+
+	def send_admin_details admin, dam
+		@name = admin.name
+		@email = admin.email
+		@password = admin.password
+		@dam_name = dam.name_of_dam
+		mail(to: @email, subject: 'Welcome Admin! Here are your credentials to manage your website')
+	end
 end
