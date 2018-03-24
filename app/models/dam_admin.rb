5d7f4c21d189ee9981bee2f2a5bf548607bd9e6a
Naman Gupta <01namangupta@gmail.com>
2018-03-24 13:16:06 +0530

    Changed admin model a=name to dm admin which were causing collision

--- app/models/dam_admin.rb
+++ app/models/dam_admin.rb
@@ -1,2 +1,14 @@
 class DamAdmin < ApplicationRecord
+	has_many :dam_admin_session_tokens
+	belongs_to :dam
+
+	after_create :send_details
+
+	# validates :password, :dam_id, :name, presence: true
+
+	# validates :email, uniqueness: true, presence: true
+
+	def send_details
+		AdminMailer.send_admin_details(self, dam).deliver_later
+	end
 end
